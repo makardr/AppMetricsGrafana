899cfdf1754270724cc121b9875710fd8ae40314
makardr <68661310+makardr@users.noreply.github.com>
2024-01-19 14:42:34 +0200

    Added docker container settings

--- AppMetricsGrafana/Program.cs
+++ AppMetricsGrafana/Program.cs
@@ -1,10 +1,8 @@
-using System.Net.Http.Headers;
 using System.Text;
 using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
 using AppMetricsGrafana.Metrics;
-
-
-var apiCode = "";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +11,7 @@ var metricsBuilder = new MetricsBuilder()
         .OutputMetrics.AsPlainText()
         .OutputMetrics.AsJson()
         .OutputMetrics.AsPrometheusPlainText()
-        .OutputMetrics.AsPrometheusProtobuf()
-    ;
+        .OutputMetrics.AsPrometheusProtobuf();
 
 var metrics = metricsBuilder.Build();
 
@@ -23,13 +20,13 @@ builder.Services.AddMetrics(metrics);
 builder.WebHost.UseMetricsWebTracking();
 
 //Output to /metrics
-// builder.WebHost.UseMetrics(options => { options.EndpointOptions = endpointOptions =>
-// {
-//     // Output of the metrics text version, protobuff is more efficient
-//     endpointOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
-//     endpointOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
-//     endpointOptions.EnvironmentInfoEndpointEnabled = false;
-// }; });
+builder.WebHost.UseMetrics(options => { options.EndpointOptions = endpointOptions =>
+{
+    // Output of the metrics text version, protobuff is more efficient
+    endpointOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+    endpointOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+    endpointOptions.EnvironmentInfoEndpointEnabled = false;
+}; });
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -56,38 +53,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-
-var snapshot = metrics.Snapshot.Get();
-
-
-foreach (var formatter in metrics.OutputMetricsFormatters)
-{
-    using (var stream = new MemoryStream())
-    {
-        await formatter.WriteAsync(stream, snapshot);
-        var httpContent = new ByteArrayContent(stream.ToArray());
-        //Output result as text in the console
-        var result = Encoding.UTF8.GetString(stream.ToArray());
-        Console.WriteLine(result);
-
-        SendRequest(httpContent);
-    }
-}
-
-
+    
 app.Run();
 
 
-metrics.Measure.Counter.Increment(MetricsRegistry.ExampleCounterOptions, 1);
 
-async void SendRequest(HttpContent httpContent)
-{
-    HttpClient client = new HttpClient();
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiCode);
-    HttpResponseMessage response =
-        await client.PostAsync("https://metrics.cockpit.fr-par.scw.cloud/api/v1/push", httpContent);
-    Console.WriteLine(response.StatusCode);
-    string responseContent = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseContent);
-}
+
