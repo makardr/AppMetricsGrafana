84431a0fb9e3a545b7cecf9426ec3a8bfdd9fae7
makardr <68661310+makardr@users.noreply.github.com>
2024-01-22 18:04:33 +0200

    Minimal AppMetrics Grafana agent example

--- AppMetricsGrafana/Program.cs
+++ AppMetricsGrafana/Program.cs
@@ -1,32 +1,33 @@
-using System.Text;
+
 using App.Metrics;
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
-using AppMetricsGrafana.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//Metrics
 var metricsBuilder = new MetricsBuilder()
-        .Report.ToConsole()
-        .OutputMetrics.AsPlainText()
-        .OutputMetrics.AsJson()
-        .OutputMetrics.AsPrometheusPlainText()
         .OutputMetrics.AsPrometheusProtobuf();
 
 var metrics = metricsBuilder.Build();
 
-builder.Services.AddMetrics(metrics);
-
+//Enable web stats metrics
 builder.WebHost.UseMetricsWebTracking();
 
 //Output to /metrics
 builder.WebHost.UseMetrics(options => { options.EndpointOptions = endpointOptions =>
 {
-    // Output of the metrics text version, protobuff is more efficient
-    endpointOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
     endpointOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
     endpointOptions.EnvironmentInfoEndpointEnabled = false;
 }; });
+
+builder.Services.AddMetrics(metrics);
+
+
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -34,8 +35,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// AddMetrics
-builder.Services.AddMetrics();
+
 
 
 var app = builder.Build();
@@ -47,6 +47,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
